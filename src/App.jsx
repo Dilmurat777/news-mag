3a4d7ce12e081fca308c9b8291e736e880f893f0
user.email <mrdilmurat777@gmail.com>
2025-07-18 22:10:07 +0600

    feat: add API data and display card items

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-
+import { useState } from 'react';
+import Navbar from './components/Navbar';
+import NewsBoard from './components/NewsBoard';
 
 const App = () => {
+  const [category, setCategory] = useState('general');
   return (
     <div>
-      
+      <Navbar setCategory={setCategory} />
+      <div style={{ maxWidth: '1430px', margin: '0 auto'}}>
+        <NewsBoard category={category} />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/components/NewsBoard.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+import NewsItem from './NewsItem';
+
+const NewsBoard = ({ category }) => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    let url = `https://newsapi.org/v2/top-headlines?country=us&category=${category}&apiKey=${
+      import.meta.env.VITE_API_KEY
+    }`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((data) => setArticles(data.articles));
+  }, [category]);
+
+  return (
+    <div>
+      <h2 className="text-center">
+        Latest <span className="badge bg-danger">News</span>
+      </h2>
+      {articles.map((articles) => {
+        return <NewsItem key={articles.url} articles={articles} />;
+      })}
+    </div>
+  );
+};
+
+export default NewsBoard;
